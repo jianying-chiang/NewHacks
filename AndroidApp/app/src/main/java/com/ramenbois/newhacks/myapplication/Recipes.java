c904ef23aec20ea853b15a3059658fc4ed9310e5
Grace Hu <gracek.hu@mail.utoronto.ca>
2019-03-23 15:39:00 -0400

    Added Recipes.java

---
+++ AndroidApp/app/src/main/java/com/ramenbois/newhacks/myapplication/Recipes.java
@@ -0,0 +1,14 @@
+package com.ramenbois.newhacks.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Recipes extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+
+
+    }
+}
