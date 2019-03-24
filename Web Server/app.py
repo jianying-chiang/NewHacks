d2ddec849dd8c9b4b73523e922082a714513fc46
Your Name <you@example.com>
2019-03-24 02:36:46 -0400

    updates

--- Web Server/app.py
+++ Web Server/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 
+from todo import Todo
 
 app = Flask(__name__)
 api = Api(app)
 
+api.add_resource(Todo.insert_ingredient, "/insert/<ingredient>")
+api.add_resource(Todo.get, "/get")
 
 if __name__ == "__main__":
     app.run()
--- Web Server/todo.py
+++ Web Server/todo.py
@@ -1,4 +1,19 @@
-from flask import Restful
+from flask_restful import Resource
 import sqlite3
 
 db = sqlite3.connect('data/ingredients.db')
+
+class Todo(Resource):
+
+    def get(self):
+        cursor.execute('''SELECT ingredients from fridge_items''')
+        ingredients = course.fetchAll()
+        return ingredients, 200
+
+
+    #def insert_ingredient(self, ingredient):
+        DATABASE = '/data/ingredients.db'
+        with sql.connect(DATABASE) as con:
+            cur = con.cursor
+            cur.execute('INSERT INTO fridge_items(ingredient) VALUES (?)', (ingredient))
+            con.commit()
